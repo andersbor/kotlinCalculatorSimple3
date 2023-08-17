c0e0527ffa7efd11a324115212fdfc3490a57d22
Anders Børjesson <anbo@zealand.dk>
2023-08-17 14:38:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculatorsimple/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.calculatorsimple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.calculatorsimple.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        binding.buttonCalculate.setOnClickListener {
+            calculate()
+        }
+
+        /* binding.edittextNumber1.setOnKeyListener { view, i, keyEvent ->
+             calculate()
+             false
+         }
+
+         binding.edittextNumber2.setOnKeyListener { view, i, keyEvent ->
+             calculate()
+             false
+         } */
+
+    }
+
+    private fun calculate() {
+        val number1str = binding.edittextNumber1.text.toString().trim()
+        val number2str = binding.edittextNumber2.text.toString().trim()
+        if (number1str.isEmpty()) {
+            binding.edittextNumber1.error = "No number"
+            return
+        }
+        if (number2str.isEmpty()) {
+            binding.edittextNumber2.error = "No number"
+            return
+        }
+        val number1 = number1str.toDouble()
+        val number2 = number2str.toDouble()
+        val result = number1 + number2
+        val resultStr = result.toString()
+        binding.textviewResult.text = resultStr
+    }
+}
